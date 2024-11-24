435048cf4715f74c6165717671077809e8f02728
Gledson <g1edson@hotmail.com>
2024-11-24 09:51:36 -0400

    Segundo exemplo simples de promise

---
+++ promise_02.js
@@ -0,0 +1,10 @@
+const primeiroElemento = arrayOuString => arrayOuString[0]
+
+
+const promessa = new Promise(resolve => {
+    resolve(['Ana', 'Bia', 'Carlos', 'Daniel'])
+})
+promessa.then(array => primeiroElemento(array))
+        .then(elemento => primeiroElemento(elemento))
+        .then(letra => console.log(letra))
+console.log('Fim')
