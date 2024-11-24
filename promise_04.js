5ebe2c4e7cd701a3213212287406e19fce905231
Gledson <g1edson@hotmail.com>
2024-11-24 10:10:01 -0400

    Quarto exemplo simples de promise

--- promise_04.js
+++ promise_04.js
@@ -1,14 +1,30 @@
 // callback ou callback hell :)
 
-setTimeout(function () {
-    console.log('Executando callback 1...')
-    setTimeout(function () {
-        console.log('Executando callback 2...')
-        setTimeout(function () {
-            console.log('Executando callback 3...')
-            setTimeout(function () {
-                console.log('Executando callback 4...')
-            },2000)
-        },2000)
-    },2000)
-}, 2000)
+// setTimeout(function () {
+//     console.log('Executando callback 1...')
+//     setTimeout(function () {
+//         console.log('Executando callback 2...')
+//         setTimeout(function () {
+//             console.log('Executando callback 3...')
+//             setTimeout(function () {
+//                 console.log('Executando callback 4...')
+//             },2000)
+//         },2000)
+//     },2000)
+// }, 2000)
+
+// Promise
+
+function esperarPor(tempo = 2000) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log('Executando promise...')
+            resolve()
+        }, tempo)
+    })
+}
+
+esperarPor()
+    .then(esperarPor)
+    .then(esperarPor)
+    .then(esperarPor)
